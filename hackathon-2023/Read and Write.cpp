52deee83e0ac676dacc67e5a4f0be2c83cf81a7c
STVR1 <veksha.glebka@mail.ru>
2023-02-19 19:43:29 +0300

    Допилил считывание. Исправил нюансы. Добавил инпут качественный

--- hackathon-2023/Read and Write.cpp
+++ hackathon-2023/Read and Write.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
+п»ї#include <iostream>
 #include <vector>
 #include <string>
 #include<fstream>
-
+#include<regex>
 
 using namespace std;
 
@@ -15,14 +15,15 @@ enum class Difficulty {
 enum class Subject {
     RPIIS,
     MATH,
-    HISTORY
+    HISTORY,
+    NONCLASSIFIED
 };
 
 class Question
 {
 public:
     Difficulty difficulty = Difficulty::MIDDLE;
-    Subject subject = Subject::RPIIS;
+    Subject subject = Subject::NONCLASSIFIED;
 
     pair<string, string> task{ "Empty question", "" };
 
@@ -36,10 +37,10 @@ class TestQuestion : Question
 public:
 
     vector<pair<string, string>> options{ { "", "" }, { "", "" } };
-    vector<pair<string, string>>::iterator correctAnswer = options.begin();
+    unsigned short correctAnswer = 0;
 
 
-    bool isAnswerRight(const vector<pair<string, string>>::iterator& selectedAnswer) {
+    bool isAnswerRight(const unsigned short& selectedAnswer) {
         return selectedAnswer == correctAnswer;
     }
 };
@@ -49,7 +50,7 @@ class OpenQuestion : Question
     string correctAnswer = "Empty correct answer";
 
     float getAnswerCorrentness(string answer) {
-        // Тут должна быть хуйня из питона которая сравнивает строки по значению
+        // РўСѓС‚ РґРѕР»Р¶РЅР° Р±С‹С‚СЊ С…СѓР№РЅСЏ РёР· РїРёС‚РѕРЅР° РєРѕС‚РѕСЂР°СЏ СЃСЂР°РІРЅРёРІР°РµС‚ СЃС‚СЂРѕРєРё РїРѕ Р·РЅР°С‡РµРЅРёСЋ
     }
 };
 
@@ -64,118 +65,118 @@ struct Set {
 
 void read(vector<Set>& test) {
 
-    bool correct = false;
-
-    bool only_pic = false;
-
+    bool correct = false,
+        only_pic = false,
+        photo = false, 
+        need = false;
     string path = "input.txt";
     ifstream file(path);
-
+    regex check_1("([(]{1}[1-3]{1}[)]{1})");
+    regex check_2("([a-zA-Z]{1}[:]{1}[\\\\]{1})");
+    smatch check;
     if (file.is_open()) {
-        string temp;
-        size_t j = -1, i = -1;
+        string temp, buf;
+        unsigned short j = -1, i = -1;
         while (getline(file, temp)) {
-
-            if (temp == "HISTORY" || temp == "MATH" || temp == "RPIIS") {
+            
+            if (temp == "HISTORY" || temp == "MATH" || temp == "RPIIS" ) {
+                need = false;
+                only_pic = false;
+                buf = "";
                 i++, j = -1;
                 test.resize(i + 1);
-                only_pic = true;
+                
                 if (temp == "HISTORY") test[i].question.subject = Subject::HISTORY;
                 else if (temp == "RPIIS") test[i].question.subject = Subject::RPIIS;
                 else if (temp == "MATH") test[i].question.subject = Subject::MATH;
-                getline(file, temp);
-                if (temp == "(1)")test[i].question.difficulty = Difficulty::EASY;
-                else if (temp == "(2)")test[i].question.difficulty = Difficulty::MIDDLE;
-                else if (temp == "(3)") test[i].question.difficulty = Difficulty::HARD;
-                getline(file, temp);
-                test[i].question.task.first = temp;
+                
+                
             }
-            else if (temp[0] == '(') {
-
-                if (test[i].testquestion.options.size() != 0) {
-
+            else if (regex_search(temp, check, check_1)) {
+                if (need == true) {
+                    only_pic = false;
+                    buf = "";
+                    need = false;
                     j = -1;
                     i++;
                     test.resize(i + 1);
-                    only_pic = true;
                 }
-
-                if (temp == "(1)")test[i].question.difficulty = Difficulty::EASY;
-                else if (temp == "(2)")test[i].question.difficulty = Difficulty::MIDDLE;
-                else if (temp == "(3)") test[i].question.difficulty = Difficulty::HARD;
-                getline(file, temp);
+                
+                if (check.str() == "(1)")test[i].question.difficulty = Difficulty::EASY;
+                else if (check.str() == "(2)")test[i].question.difficulty = Difficulty::MIDDLE;
+                else if (check.str() == "(3)") test[i].question.difficulty = Difficulty::HARD;
+                
                 test[i].question.task.first = temp;
 
             }
 
-            else if (temp == "[") {
-                getline(file, temp);
-                test[i].question.task.second = temp;
-            }
-            else if (temp == "*") correct = true;
-            else if (temp == "[[") {
-
+            else if (regex_search(temp, check, check_2)) {
+                
+                if (photo == false && buf.empty()) {
+                    buf = temp;
+                    photo = true;
+                }
+                else if (photo == true) {
+                    only_pic = true;
+                    photo = false;
+                }
                 if (only_pic == true) {
 
+                    need = true;
                     j++;
+                    if (correct == true) {
+                        test[i].testquestion.correctAnswer = j;
+                        correct = false;
+                    }
                     test[i].testquestion.options.resize(j + 1);
-
+                    if (j == 0) {
+                        test[i].testquestion.options[j].second = buf;
+                        j++;
+                        test[i].testquestion.options.resize(j + 1);
+                    }
+                    test[i].testquestion.options[j].second = temp;
                 }
-                getline(file, temp);
-                test[i].testquestion.options[j].second = temp;
-
-
-
-                //Здесь я приваиваю
-                if (correct == true) {
-                    test[i].testquestion.correctAnswer = test[i].testquestion.options.begin() + j;
-                    
-                    // DEBUG
-                    cout << "Read corrent answer " << (*(test[i].testquestion.correctAnswer)).first << "\n";
-
-                    correct = false;
-                }
-
-
-
+                
+               
             }
+            else if (temp == "*") correct = true;
+           
             else if (temp.empty()) continue;
             else {
+
+
+                if (photo == true) {
+                    test[i].question.task.second = buf;
+                    photo = false;
+                }
+
+
+                need = true;
                 j++;
                 test[i].testquestion.options.resize(j + 1);
                 test[i].testquestion.options[j].first = temp;
-                only_pic = false;
-
-
-
-                //Здесь я присваиваю
+                
+               
 
                 if (correct == true) {
-                    test[i].testquestion.correctAnswer = test[i].testquestion.options.begin() + j;
-
-                    // DEBUG
-                    cout << "Read corrent answer " << (*(test[i].testquestion.correctAnswer)).first << "\n";
-
+                    test[i].testquestion.correctAnswer =j;
                     correct = false;
                 }
 
 
 
             }
-
+            
         }
         file.close();
     }
-    else {
-        cout << "fuck\n";
-    }
 }
+
 
 
 void save(vector<Set>& test) {
     string path = "inp.txt";
     ofstream file(path);
-
     if (file.is_open()) {
         for (const auto& currentQuestion : test) {
 
@@ -183,27 +184,16 @@ void save(vector<Set>& test) {
             case Subject::HISTORY: file << "HISTORY" << endl; break;
             case Subject::RPIIS: file << "RPIIS" << endl; break;
             case Subject::MATH: file << "MATH" << endl; break;
-            default: file << "NONSPECIFIED" << endl; break;
             }
-
-            switch (currentQuestion.question.difficulty) {
-            case Difficulty::EASY: file << "(" << 1 << ")" << endl; break;
-            case Difficulty::MIDDLE: file << "(" << 2 << ")" << endl; break;
-            case Difficulty::HARD: file << "(" << 3 << ")" << endl; break;
-            }
-
             file << currentQuestion.question.task.first << endl;
-            if (!currentQuestion.question.task.second.empty()) file << "[" << endl << currentQuestion.question.task.second << endl;
-
+            if (!currentQuestion.question.task.second.empty()) file << currentQuestion.question.task.second << endl;
+            unsigned short j = 0;
             for (const auto& option : currentQuestion.testquestion.options) {
-
-                cout << option.first << endl;
-                if (option == *(currentQuestion.testquestion.correctAnswer)) file << "*" << endl;
-
-               /* if (test[i].testquestion.options[j].first.empty()) file << "[[" << endl << test[i].testquestion.options[j].second << endl;
-                else if (test[i].testquestion.options[j].second.empty()) file << test[i].testquestion.options[j].first << endl;
-                else file << test[i].testquestion.options[j].first << endl << "[[" << test[i].testquestion.options[j].second << endl;*/
-
+                if (currentQuestion.testquestion.correctAnswer == j) file << "*" << endl;
+                if (option.first.empty()) file << option.second << endl;
+                else if (option.second.empty()) file << option.first << endl;
+                else file << option.first << endl << option.second << endl;
+                j++;
             }
 
         }
@@ -215,9 +205,19 @@ int main() {
     setlocale(LC_ALL, "Russian");
     vector<Set> test;
     read(test);
-
-
-
+    /*for (int i = 0; i < test.size(); i++) {
+       if (test[i].question.difficulty == Difficulty::EASY)cout << "1" << endl;
+       else if (test[i].question.difficulty == Difficulty::MIDDLE)cout << "2" << endl;
+       else cout << "3" << endl;
+       cout << test[i].question.task.first<<"\t";
+       if (!test[i].question.task.second.empty())  cout << test[i].question.task.second << endl;
+       else cout << endl;
+       for (int j = 0; j < test[i].testquestion.options.size(); j++) {
+           if (test[i].testquestion.correctAnswer == j) cout << "Correct answer : ";
+           if (!test[i].testquestion.options[j].first.empty())cout << test[i].testquestion.options[j].first << endl;
+           if (!test[i].testquestion.options[j].second.empty())cout << test[i].testquestion.options[j].second << endl;
+       }
+    }*/ //SHOWING
     save(test);
     return 0;
 }
