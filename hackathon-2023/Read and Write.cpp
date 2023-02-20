86fc500b10c898c3239fddd855a9a1fb7f0a3ec3
STVR1 <veksha.glebka@mail.ru>
2023-02-20 21:21:34 +0300

    Надо только кодировка

--- hackathon-2023/Read and Write.cpp
+++ hackathon-2023/Read and Write.cpp
@@ -1,9 +1,18 @@
-﻿#include <iostream>
-#include <vector>
-#include <string>
-#include<fstream>
-#include<regex>
 
+
+#include <QCoreApplication>
+#include <QLocale>
+#include <QTranslator>
+#include<QTextStream>
+#include <QIODevice>
+
+#include <iostream>
+#include <vector>
+#include<QString>
+#include<QFile>
+#include <QRegularExpression>
+#include<QRegularExpressionMatch>
+#include <QDebug>
 using namespace std;
 
 enum class Difficulty {
@@ -19,24 +28,29 @@ enum class Subject {
     NONCLASSIFIED
 };
 
+enum class Questions {
+    TEST,
+    OPEN
+};
+
 class Question
 {
 public:
     Difficulty difficulty = Difficulty::MIDDLE;
     Subject subject = Subject::NONCLASSIFIED;
 
-    pair<string, string> task{ "Empty question", "" };
+    pair<QString, QString> task{ "", "" };
 
 };
 
 
-class TestQuestion : Question
+class TestQuestion : public Question
 {
 
 
 public:
 
-    vector<pair<string, string>> options{ { "", "" }, { "", "" } };
+    vector<pair<QString, QString>> options{ { "", "" }, { "", "" } };
     unsigned short correctAnswer = 0;
 
 
@@ -47,52 +61,65 @@ public:
 
 class OpenQuestion : Question
 {
-    string correctAnswer = "Empty correct answer";
+public:
+    QString correctAnswer = "";
 
-    float getAnswerCorrentness(string answer) {
+    /*float getAnswerCorrentness(QString answer) {
         // Тут должна быть хуйня из питона которая сравнивает строки по значению
-    }
+    }*/
+};
+
+class TestElement
+{
+public:
+    OpenQuestion openQuestion;
+    TestQuestion testQuestion;
+
+    Questions questions = Questions::TEST;
 };
 
 
-struct Set {
 
-    Question question;
-    TestQuestion testquestion;
-    OpenQuestion openquestion;
 
-};
-
-void read(vector<Set>& test) {
+void read(vector<TestElement>& test) {
 
     bool correct = false,
         only_pic = false,
-        photo = false, 
-        need = false;
-    string path = "input.txt";
-    ifstream file(path);
-    regex check_1("([(]{1}[1-3]{1}[)]{1})");
-    regex check_2("([a-zA-Z]{1}[:]{1}[\\\\]{1})");
-    smatch check;
-    if (file.is_open()) {
-        string temp, buf;
+        photo = false,
+        need = false,
+            open=false;
+    QString path = "c:\\users\\user\\desktop\\input.txt";
+    QFile file(path);
+    static QRegularExpression check_1("([(]{1}[1-3]{1}[)]{1})");
+    static QRegularExpression check_2("([a-zA-Z]{1}[:]{1}[\\\\]{1})");
+
+
+    if (file.open(QIODevice::ReadOnly |QIODevice::Text)) {
+
+
+        QString temp, buf;
         unsigned short j = -1, i = -1;
-        while (getline(file, temp)) {
-            
+        QTextStream s(&file);
+        while (!s.atEnd()) {
+
+            temp=s.readLine();
+           // qDebug()<<temp;
+
+
             if (temp == "HISTORY" || temp == "MATH" || temp == "RPIIS" ) {
+
                 need = false;
                 only_pic = false;
                 buf = "";
                 i++, j = -1;
                 test.resize(i + 1);
-                
-                if (temp == "HISTORY") test[i].question.subject = Subject::HISTORY;
-                else if (temp == "RPIIS") test[i].question.subject = Subject::RPIIS;
-                else if (temp == "MATH") test[i].question.subject = Subject::MATH;
-                
-                
+
+                if (temp == "HISTORY") test[i].testQuestion.subject = Subject::HISTORY;
+                else if (temp == "RPIIS") test[i].testQuestion.subject = Subject::RPIIS;
+                else if (temp == "MATH") test[i].testQuestion.subject = Subject::MATH;
+
             }
-            else if (regex_search(temp, check, check_1)) {
+            else if (check_1.match(temp).hasMatch()) {
                 if (need == true) {
                     only_pic = false;
                     buf = "";
@@ -101,18 +128,18 @@ void read(vector<Set>& test) {
                     i++;
                     test.resize(i + 1);
                 }
-                
-                if (check.str() == "(1)")test[i].question.difficulty = Difficulty::EASY;
-                else if (check.str() == "(2)")test[i].question.difficulty = Difficulty::MIDDLE;
-                else if (check.str() == "(3)") test[i].question.difficulty = Difficulty::HARD;
-                
-                test[i].question.task.first = temp;
+
+                if (check_1.match(temp).captured() == "(1)")test[i].testQuestion.difficulty = Difficulty::EASY;
+                else if (check_1.match(temp).captured() == "(2)")test[i].testQuestion.difficulty = Difficulty::MIDDLE;
+                else if (check_1.match(temp).captured() == "(3)") test[i].testQuestion.difficulty = Difficulty::HARD;
+
+                test[i].testQuestion.task.first = temp;
 
             }
 
-            else if (regex_search(temp, check, check_2)) {
-                
-                if (photo == false && buf.empty()) {
+            else if (check_2.match(temp).hasMatch()) {
+
+                if (photo == false && buf.isEmpty()) {
                     buf = temp;
                     photo = true;
                 }
@@ -125,76 +152,94 @@ void read(vector<Set>& test) {
                     need = true;
                     j++;
                     if (correct == true) {
-                        test[i].testquestion.correctAnswer = j;
+                        test[i].testQuestion.correctAnswer = j;
                         correct = false;
                     }
-                    test[i].testquestion.options.resize(j + 1);
+                    test[i].testQuestion.options.resize(j + 1);
                     if (j == 0) {
-                        test[i].testquestion.options[j].second = buf;
+                        test[i].testQuestion.options[j].second = buf;
                         j++;
-                        test[i].testquestion.options.resize(j + 1);
+                        test[i].testQuestion.options.resize(j + 1);
                     }
-                    test[i].testquestion.options[j].second = temp;
+                    test[i].testQuestion.options[j].second = temp;
                 }
-                
-               
+
+
             }
             else if (temp == "*") correct = true;
-           
-            else if (temp.empty()) continue;
+            else if(temp=="**") open=true;
+            else if (temp.isEmpty()) {
+
+                continue;
+
+            }
+            else if(open==true){
+                  test[i].openQuestion.correctAnswer=temp;
+                  open=false;
+
+                  need=true;
+            }
             else {
 
 
                 if (photo == true) {
-                    test[i].question.task.second = buf;
+                    test[i].testQuestion.task.second = buf;
                     photo = false;
                 }
 
 
                 need = true;
                 j++;
-                test[i].testquestion.options.resize(j + 1);
-                test[i].testquestion.options[j].first = temp;
-                
-               
+                test[i].testQuestion.options.resize(j + 1);
+                test[i].testQuestion.options[j].first = temp;
+
+
 
                 if (correct == true) {
-                    test[i].testquestion.correctAnswer =j;
+                    test[i].testQuestion.correctAnswer =j;
                     correct = false;
                 }
 
 
 
             }
-            
+
         }
         file.close();
     }
+
 }
 
 
 
-void save(vector<Set>& test) {
-    string path = "inp.txt";
-    ofstream file(path);
-    if (file.is_open()) {
+void save(vector<TestElement>& test) {
+    QString path = "c:\\users\\user\\desktop\\inp.txt";
+    QFile file(path);
+    QTextStream st(&file);
+    if (file.open(QFile::WriteOnly)) {
         for (const auto& currentQuestion : test) {
 
-            switch (currentQuestion.question.subject) {
-            case Subject::HISTORY: file << "HISTORY" << endl; break;
-            case Subject::RPIIS: file << "RPIIS" << endl; break;
-            case Subject::MATH: file << "MATH" << endl; break;
+            switch (currentQuestion.testQuestion.subject) {
+            case Subject::HISTORY: st<<"HISTORY\n"; break;
+            case Subject::RPIIS: st<<"RPIIS\n"; break;
+            case Subject::MATH: st<<"MATH\n"; break;
+            case Subject::NONCLASSIFIED: break;
             }
-            file << currentQuestion.question.task.first << endl;
-            if (!currentQuestion.question.task.second.empty()) file << currentQuestion.question.task.second << endl;
+
+            st<<currentQuestion.testQuestion.task.first<<"\n";
+
+            if (!currentQuestion.testQuestion.task.second.isEmpty()) st << currentQuestion.testQuestion.task.second << "\n";
             unsigned short j = 0;
-            for (const auto& option : currentQuestion.testquestion.options) {
-                if (currentQuestion.testquestion.correctAnswer == j) file << "*" << endl;
-                if (option.first.empty()) file << option.second << endl;
-                else if (option.second.empty()) file << option.first << endl;
-                else file << option.first << endl << option.second << endl;
+            if(currentQuestion.openQuestion.correctAnswer.isEmpty()){
+            for (const auto& option : currentQuestion.testQuestion.options) {
+                if (currentQuestion.testQuestion.correctAnswer == j) st << "*\n";
+                if (option.first.isEmpty()) st << option.second <<"\n";
+                else if (option.second.isEmpty()) st<<option.first<<"\n";
+                else st << option.first << "\n" << option.second << "\n";
                 j++;
             }
+            }
+            else st<<"**\n"<<currentQuestion.openQuestion.correctAnswer<<"\n";
 
         }
         file.close();
@@ -202,22 +247,32 @@ void save(vector<Set>& test) {
 }
 
 int main() {
-    setlocale(LC_ALL, "Russian");
-    vector<Set> test;
+
+
+vector<TestElement> test;
+
     read(test);
-    /*for (int i = 0; i < test.size(); i++) {
-       if (test[i].question.difficulty == Difficulty::EASY)cout << "1" << endl;
-       else if (test[i].question.difficulty == Difficulty::MIDDLE)cout << "2" << endl;
+/*
+    for (int i = 0; i < test.size(); i++) {
+       if (test[i].testQuestion.difficulty == Difficulty::EASY)cout << "1" << endl;
+       else if (test[i].testQuestion.difficulty == Difficulty::MIDDLE)cout << "2" << endl;
        else cout << "3" << endl;
-       cout << test[i].question.task.first<<"\t";
-       if (!test[i].question.task.second.empty())  cout << test[i].question.task.second << endl;
-       else cout << endl;
-       for (int j = 0; j < test[i].testquestion.options.size(); j++) {
-           if (test[i].testquestion.correctAnswer == j) cout << "Correct answer : ";
-           if (!test[i].testquestion.options[j].first.empty())cout << test[i].testquestion.options[j].first << endl;
-           if (!test[i].testquestion.options[j].second.empty())cout << test[i].testquestion.options[j].second << endl;
+       qDebug()<< test[i].testQuestion.task.first<<"\t";
+       if (!test[i].testQuestion.task.second.isEmpty())  qDebug()<<test[i].testQuestion.task.second << "\n";
+       else qDebug()<<"\n";
+       if(test[i].openQuestion.correctAnswer.isEmpty()){
+       for (int j = 0; j < test[i].testQuestion.options.size(); j++) {
+           if (test[i].testQuestion.correctAnswer == j) cout << "Correct answer : ";
+           if (!test[i].testQuestion.options[j].first.isEmpty())qDebug()<<test[i].testQuestion.options[j].first;
+           if (!test[i].testQuestion.options[j].second.isEmpty())qDebug()<<test[i].testQuestion.options[j].second<<"\n";
        }
-    }*/ //SHOWING
+       }
+       else qDebug()<<test[i].openQuestion.correctAnswer<<"\n";
+    }*/
+
     save(test);
+
     return 0;
 }
+
+
