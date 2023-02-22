9a4f64b11b1bc427bd157ca046b05c3101443c76
STVR1 <veksha.glebka@mail.ru>
2023-02-23 02:18:50 +0300

    Finally

--- hackathon-2023/filesystem.cpp
+++ hackathon-2023/filesystem.cpp
@@ -9,168 +9,175 @@ void FileSystem::readQuestions(vector<TestElement> &test, QString path)
 {
     bool correct = false,
             only_pic = false,
-            photo = false,
             need = false,
             open=false;
 
-        QFile file(path);
-        static QRegularExpression check_1("([(]{1}[1-3]{1}[)]{1})");
-
-
-        if (file.open(QIODevice::ReadOnly |QIODevice::Text))
-        {
-            QString temp, buf;
-            QChar c;
-            unsigned short j = -1, i = -1;
-            QTextStream s(&file);
-            while (!s.atEnd())
-            {
-                temp = s.readLine();
-                c=temp[0];
-
-                if (temp == "HISTORY" || temp == "MATH" || temp == "RPIIS" ) {
-
-                    need = false;
-                    only_pic = false;
-                    buf = "";
-                    i++, j = -1;
-                    test.resize(i + 1);
-
-                    if (temp == "HISTORY") {
-                        test[i].testQuestion.subject = Subject::HISTORY;
-                        test[i].openQuestion.subject = Subject::HISTORY;
-                    } else if (temp == "RPIIS") {
-                        test[i].testQuestion.subject = Subject::RPIIS;
-                        test[i].openQuestion.subject = Subject::RPIIS;
-                    }
-                    else if (temp == "MATH") {
-                        test[i].testQuestion.subject = Subject::MATH;
-                        test[i].openQuestion.subject = Subject::MATH;
-                    }
-                }
-                else if (check_1.match(temp).hasMatch()) {
-                    if (need == true) {
-                        only_pic = false;
-                        buf = "";
-                        need = false;
-                        j = -1;
-                        i++;
-                        test.resize(i + 1);
-                    }
-                    temp=s.readLine();
-                    if (check_1.match(temp).captured() == "(1)") {
-                        test[i].testQuestion.difficulty = Difficulty::EASY;
-                        test[i].openQuestion.difficulty = Difficulty::EASY;
-                    } else if (check_1.match(temp).captured() == "(2)") {
-                        test[i].testQuestion.difficulty = Difficulty::MIDDLE;
-                        test[i].openQuestion.difficulty = Difficulty::MIDDLE;
-                    } else if (check_1.match(temp).captured() == "(3)") {
-                        test[i].testQuestion.difficulty = Difficulty::HARD;
-                        test[i].openQuestion.difficulty = Difficulty::HARD;
-                    }
-                    test[i].testQuestion.task.first = temp;
-                    test[i].openQuestion.task.first = temp;
-                }
-
-                else if (!c.isDigit() && c!='*') {
-
-                    if (photo == false && buf.isEmpty()) {
-                        buf = temp;
-                        photo = true;
-                    }
-                    else if (photo == true) {
-                        only_pic = true;
-                        photo = false;
-                    }
-                    if (only_pic == true) {
-
-                        need = true;
-                        j++;
-                        if (correct == true) {
-                            test[i].testQuestion.correctAnswer = j;
-                            correct = false;
-                        }
-                        test[i].testQuestion.options.resize(j + 1);
-                        if (j == 0) {
-                            test[i].testQuestion.options[j].second = buf;
-                            j++;
-                            test[i].testQuestion.options.resize(j + 1);
-                        }
-                        test[i].testQuestion.options[j].second = temp;
-                    }
-
-                }
-                else if (temp == "*") correct = true;
-                    else if(temp=="**") {
-                        test[i].questions = Questions::OPEN;
-                        temp=s.readLine();
-                        test[i].openQuestion.correctAnswer = temp;
-
-                        need=true;
-                    }
-
-                else if (temp.isEmpty()) {
-                    continue;
-                }
-
-                else
-                {
-                    if (photo == true) {
-                        test[i].testQuestion.task.second = buf;
-                        test[i].openQuestion.task.second = buf;
-                        photo = false;
-                    }
-
-                    need = true;
-                    j++;
-                    test[i].testQuestion.options.resize(j + 1);
-                    test[i].testQuestion.options[j].first = temp;
-
-                    if (correct == true) {
-                        test[i].testQuestion.correctAnswer = j;
-                        correct = false;
-                    }
-                }
-            }
-            file.close();
-        }
-}
-
-void FileSystem::saveQuestions(vector<TestElement> &test, QString path)
-{
     QFile file(path);
-        QTextStream st(&file);
-        if (file.open(QFile::WriteOnly)) {
-            for (const auto& currentQuestion : test)
-            {
-                switch (currentQuestion.testQuestion.subject) {
-                case Subject::HISTORY: st<<"HISTORY\n"; break;
-                case Subject::RPIIS: st<<"RPIIS\n"; break;
-                case Subject::MATH: st<<"MATH\n"; break;
-                case Subject::NONSPECIFIED: break;
-                }
-                switch(currentQuestion.testQuestion.difficulty){
-                case Difficulty::EASY: st<<"(1)\n";break;
-                case Difficulty::MIDDLE:st<<"(2)\n";break;
-                case Difficulty::HARD:st<<"(3)\n";break;
-                }
-                st<<currentQuestion.testQuestion.task.first<<"\n";
+    static QRegularExpression check_1("([(]{1}[1-3]{1}[)]{1})");
+    static QRegularExpression check_2 ("[a-zA-Z[:space:]]+[:]{1}[\\/]");
 
-                if (!currentQuestion.testQuestion.task.second.isEmpty()) st << currentQuestion.testQuestion.task.second << "\n";
-                unsigned short j = 0;
-                if(currentQuestion.openQuestion.correctAnswer.isEmpty()){
-                for (const auto& option : currentQuestion.testQuestion.options)
-                {
-                    if (currentQuestion.testQuestion.correctAnswer == j) st << "*\n";
-                    if (option.first.isEmpty()) st << option.second << '\n';
-                    else if (option.second.isEmpty()) st<<option.first << '\n';
-                    else st << option.first << '\n' << option.second << '\n';
-                    j++;
-                }
-                }
-                else st << "**\n" << currentQuestion.openQuestion.correctAnswer << '\n';
+
+    if (file.open(QIODevice::ReadOnly |QIODevice::Text)) {
+
+
+        QString temp, buf_1, buf_2;
+        unsigned short j = 0, i = -1;
+        QTextStream s(&file);
+
+        while (!s.atEnd()) {
+            temp=s.readLine();
+           if(temp=="HISTORY" || temp=="MATH" || temp=="RPISS"){
+               if(need==true){
+               i++;
+               j=0;
+               test.resize(i+1);
+               need=false;
+               only_pic=false;
+               }
+               if(temp=="HISTORY") {
+                   test[i].testQuestion.subject=Subject::HISTORY;
+                   test[i].openQuestion.subject=Subject::HISTORY;
+               }
+                   else if(temp=="MATH") {
+                   test[i].testQuestion.subject=Subject::MATH;
+                   test[i].openQuestion.subject=Subject::MATH;
+               }
+                   else if(temp=="RPIIS"){
+                   test[i].testQuestion.subject=Subject::RPIIS;
+                    test[i].openQuestion.subject=Subject::RPIIS;
+
+               }
+                       else {
+                   test[i].testQuestion.subject=Subject::NONSPECIFIED;
+                   test[i].openQuestion.subject=Subject::NONSPECIFIED;
+               }
 
             }
-            file.close();
+           else if(check_1.match(temp).hasMatch()){
+               if(need==true){
+                   i++;
+                   test.resize(i+1);
+                   j=0;
+                   need=false;
+                   only_pic=false;
+               }
+
+
+               if(check_1.match(temp).captured()=="(1)"){
+                   test[i].testQuestion.difficulty=Difficulty::EASY;
+                   test[i].openQuestion.difficulty=Difficulty::EASY;
+               }
+               else if (check_1.match(temp).captured()=="(2)"){
+                   test[i].testQuestion.difficulty=Difficulty::MIDDLE;
+                   test[i].openQuestion.difficulty=Difficulty::MIDDLE;
+               }
+               else if (check_1.match(temp).captured()=="(3)"){
+                   test[i].testQuestion.difficulty=Difficulty::HARD;
+                   test[i].openQuestion.difficulty=Difficulty::HARD;
+               }
+               temp=s.readLine();
+               buf_1=temp;
+           }
+           else if(check_2.match(temp).hasMatch()){
+               if(only_pic==false) {
+                   only_pic=true;
+                   buf_2=temp;
+               }
+               else {
+                   if(!buf_2.isEmpty()){
+                       test[i].testQuestion.task.first=buf_1;
+                       test[i].testQuestion.task.second=buf_2;
+
+                       buf_2="";
+                       buf_1="";
+                   }
+                   if(need==false)need=true;
+                   test[i].testQuestion.options.resize(j+1);
+                   test[i].testQuestion.options[j].second=temp;
+
+                   if(correct==true) {
+                       test[i].testQuestion.correctAnswer=j;
+                       correct=false;
+                   }
+                   j++;
+               }
+           }
+           else if(temp=="**"){
+               temp=s.readLine();
+               test[i].openQuestion.task.first=buf_1;
+
+               test[i].openQuestion.correctAnswer=temp;
+               if(!buf_2.isEmpty())test[i].openQuestion.task.second=buf_2;
+               buf_1="";
+               buf_2="";
+               need=true;
+           }
+           else if(temp=="*") correct=true;
+           else if(temp.isEmpty()) continue;
+           else {
+               if(only_pic==true){
+                   only_pic=false;
+                   test[i].testQuestion.task.second=buf_2;
+                   buf_2="";
+               }
+               if(!buf_1.isEmpty()){
+                   test[i].testQuestion.task.first=buf_1;
+
+                   buf_1="";
+               }
+               if(need==false)need =true;
+               test[i].testQuestion.options.resize(j+1);
+               test[i].testQuestion.options[j].first=temp;
+               if(correct==true){
+                   test[i].testQuestion.correctAnswer=j;
+                   correct=false;
+               }
+               j++;
+           }
+
+        }
+        file.close();
+    }
 }
+
+
+
+void save(vector<TestElement>& test) {
+    QFile file(path);
+    QTextStream st(&file);
+    if (file.open(QFile::WriteOnly)) {
+        for (const auto& currentQuestion : test) {
+
+            switch (currentQuestion.testQuestion.subject) {
+            case Subject::HISTORY: st<<"HISTORY\n"; break;
+            case Subject::RPIIS: st<<"RPIIS\n"; break;
+            case Subject::MATH: st<<"MATH\n"; break;
+            case Subject::NONSPECIFIED: break;
+            }
+            switch(currentQuestion.testQuestion.difficulty){
+            case Difficulty::EASY: st<<"(1)\n";break;
+            case Difficulty::MIDDLE:st<<"(2)\n";break;
+            case Difficulty::HARD:st<<"(3)\n";break;
+            }
+
+            if(currentQuestion.openQuestion.task.first.isEmpty())st<<currentQuestion.testQuestion.task.first<<"\n";
+            else st<<currentQuestion.openQuestion.task.first<<"\n";
+
+            if (!currentQuestion.testQuestion.task.second.isEmpty()) st << currentQuestion.testQuestion.task.second << "\n";
+            if (!currentQuestion.openQuestion.task.second.isEmpty()) st << currentQuestion.openQuestion.task.second << "\n";
+            unsigned short j = 0;
+            if(currentQuestion.openQuestion.correctAnswer.isEmpty()){
+            for (const auto& option : currentQuestion.testQuestion.options) {
+                if (currentQuestion.testQuestion.correctAnswer == j) st << "*\n";
+                if (option.first.isEmpty()) st << option.second <<"\n";
+                else if (option.second.isEmpty()) st<<option.first<<"\n";
+                else st << option.first << "\n" << option.second << "\n";
+                j++;
+            }
+            }
+            else st<<"**\n"<<currentQuestion.openQuestion.correctAnswer<<"\n";
+
+        }
+        file.close();
+    }
 }
